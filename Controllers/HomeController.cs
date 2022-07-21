5b6bca19d0c6d4dfb2f3217ca7c51e5fb0b6cade
cryle1205 <97998586+cryle1205@users.noreply.github.com>
2022-07-21 15:58:29 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Le_Crystal_HW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            string path = _environment.WebRootPath + "/files/LeCrystal_Resume_2022.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "LeCrystal_Resume_2022.pdf");
+        }
+
+        public IActionResult FoodieCorner()
+        {
+            return View();
+        }
+
+        public IActionResult Spotify()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
