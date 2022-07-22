62a51ec8cf879f20dd92cd9983d51fb527170173
cryle1205 <97998586+cryle1205@users.noreply.github.com>
2022-07-21 23:37:28 -0500

    Add files via upload

---
+++ Homework-5/Controllers/ProductsController.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Le_Crystal_HW5.DAL;
+using Le_Crystal_HW5.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Le_Crystal_HW5.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        [AllowAnonymous]
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("Error", new String[] { "Please specify a product to view!" });
+
+            }
+
+            Product product = await _context.Products
+                .Include(p => p.Suppliers)
+                .FirstOrDefaultAsync(m => m.ProductID == id);
+
+            if (product == null)
+            {
+                return View("Error", new String[] { "That product was not found in the database." });
+
+            }
+            
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewBag.AllSuppliers = GetAllSuppliers();
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductID,ProductName,ProductDes,Price,PType")] Product product, int[] SelectedSupplierIDs)
+        {
+            if (ModelState.IsValid == false)
+            {
+                ViewBag.AllSuppliers = GetAllSuppliers();
+                return View(product);
+            }
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+            foreach (int supplierID in SelectedSupplierIDs)
+            {
+                //find the department associated with that id
+                Supplier dbSupplier = _context.Suppliers.Find(supplierID);
+
+                //add the department to the course's list of departments and save changes
+                product.Suppliers.Add(dbSupplier);
+                _context.SaveChanges();
+            }
+
+            //Send the user to the page with all the departments
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return View("Error", new string[] { "Please specify a products to edit!" });
+            }
+
+            Product product = await _context.Products.Include(c => c.Suppliers).FirstOrDefaultAsync(p => p.ProductID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewBag.AllSuppliers = GetAllSuppliers(product);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductID,ProductName,ProductDes,Price,PType")] Product product, int[] SelectedSupplierIDs)
+        {
+            if (id != product.ProductID)
+            {
+                return View("Error", new String[] { "Please specify a known product to edit!" });
+            }
+            if (ModelState.IsValid == false) //there is something wrong
+            {
+                ViewBag.AllSuppliers = GetAllSuppliers(product);
+                return View(product);
+            }
+
+            try
+            {
+                //Find the course to edit in the database and include relevant 
+                //navigational properties
+                Product dbProduct = _context.Products
+                    .Include(c => c.Suppliers)
+                    .FirstOrDefault(c => c.ProductID == product.ProductID);
+
+                //create a list of departments that need to be removed
+                List<Supplier> SuppliersToRemove = new List<Supplier>();
+
+                //find the departments that should no longer be selected because the
+                //user removed them
+                //remember, SelectedDepartments = the list from the HTTP request (listbox)
+                foreach (Supplier supplier in dbProduct.Suppliers)
+                {
+                    //see if the new list contains the department id from the old list
+                    if (SelectedSupplierIDs.Contains(supplier.SupplierID) == false)//this department is not on the new list
+                    {
+                        SuppliersToRemove.Add(supplier);
+                    }
+                }
+
+                //remove the departments you found in the list above
+                //this has to be 2 separate steps because you can't iterate (loop)
+                //over a list that you are removing things from
+                foreach (Supplier supplier in SuppliersToRemove)
+                {
+                    //remove this course department from the course's list of departments
+                    dbProduct.Suppliers.Remove(supplier);
+                    _context.SaveChanges();
+                }
+
+                //add the departments that aren't already there
+                foreach (int supplierID in SelectedSupplierIDs)
+                {
+                    if (dbProduct.Suppliers.Any(d => d.SupplierID == supplierID) == false)//this department is NOT already associated with this course
+                    {
+                        //Find the associated department in the database
+                        Supplier dbSupplier = _context.Suppliers.Find(supplierID);
+
+                        //Add the department to the course's list of departments
+                        dbProduct.Suppliers.Add(dbSupplier);
+                        _context.SaveChanges();
+                    }
+                }
+
+                //update the course's scalar properties
+                dbProduct.Price= product.Price;
+                dbProduct.ProductName = product.ProductName;
+                dbProduct.ProductDes = product.ProductDes;
+                dbProduct.PType = product.PType;
+                //save the changes
+                _context.Products.Update(dbProduct);
+                _context.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                return View("Error", new string[] { "There was an error editing this course.", ex.Message });
+            }
+
+            //if code gets this far, everything is okay
+            //send the user back to the page with all the courses
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Products/Delete/5
+        
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductID == id);
+        }
+
+        private MultiSelectList GetAllSuppliers()
+        {
+            List<Supplier> allSuppliers = _context.Suppliers.ToList();
+
+            MultiSelectList mslAllSuppliers = new MultiSelectList(allSuppliers.OrderBy(d => d.SupplierName), "SupplierID", "SupplierName");
+
+            return mslAllSuppliers;
+        }
+
+        private MultiSelectList GetAllSuppliers(Product product)
+        {
+
+            List<Supplier> allSuppliers = _context.Suppliers.ToList();
+           
+            List<Int32> selectedSupplierIDs = new List<Int32>();
+
+            foreach (Supplier associatedSupplier in product.Suppliers)
+            {
+                selectedSupplierIDs.Add(associatedSupplier.SupplierID);
+            }
+            MultiSelectList mslAllSuppliers = new MultiSelectList(allSuppliers.OrderBy(d => d.SupplierName), "SupplierID", "SupplierName", selectedSupplierIDs);
+
+            return mslAllSuppliers;
+        }
+
+    }
+}
